cedf141c34ef232ffa3fe9a4becce1f2c87600f1
miraykkus <120058866+miraykkus@users.noreply.github.com>
2023-01-03 13:45:22 +0300

    Add files via upload

---
+++ python/hafta12/hafta12_1.py
@@ -0,0 +1,15 @@
+#selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("http://www.youtube.com") #youtube a otomatik olarak girdi
+'''assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)'''
+assert "No results found." not in driver.page_source
+
+
